ca50ceb45b0b1cc834efcdd780cfa08df1b63c40
Elicasta <130819591+Elicasta@users.noreply.github.com>
2026-08-05 19:43:06 -0400

    Load desktop and tablet polish styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,7 @@ import "./editorial-divider-system.css";
 import "./line-polish-final.css";
 import "./scripture-library-browser.css";
 import "./about-page-v2.css";
+import "./desktop-tablet-polish.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(websiteUrl),
