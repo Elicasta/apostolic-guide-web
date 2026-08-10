c50a2e3d59106a8f6ba9d5713da67fffff58a372
Elicasta <130819591+Elicasta@users.noreply.github.com>
2026-08-10 11:25:56 -0400

    Load notification center styles

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -13,6 +13,7 @@ import "./journeys.css";
 import "./relationship.css";
 import "./inbox.css";
 import "./segments.css";
+import "./notifications.css";
 import "./studio-system.css";
 
 export const dynamic = "force-dynamic";
