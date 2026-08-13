1bb857eea5302881ec709987df6024290abea7fd
Elicasta <130819591+Elicasta@users.noreply.github.com>
2026-08-13 16:45:11 -0400

    Load carousel caption suite styles

--- app/admin/publish/layout.tsx
+++ app/admin/publish/layout.tsx
@@ -3,6 +3,7 @@ import { InstagramPublishingWorkflow } from "@/instagram-publishing-workflow";
 import { MediaPublishingOverviewPortal } from "@/media-publishing-overview";
 import { PublishingRouteIntent } from "@/publishing-route-intent";
 import "../media-publishing-overview.css";
+import "../instagram-caption-suite.css";
 
 export default function PublishingLayout({ children }: { children: React.ReactNode }) {
   return <>{children}<MediaPublishingOverviewPortal/><InstagramPublishingWorkflow/><InstagramCarouselCaptionSuite/><PublishingRouteIntent/></>;
