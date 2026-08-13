072dcb96e245b2f2423f9968cf5ad8bf82d2612e
Elicasta <130819591+Elicasta@users.noreply.github.com>
2026-08-13 18:06:59 -0400

    Mount Threads publishing suite

--- app/admin/publish/layout.tsx
+++ app/admin/publish/layout.tsx
@@ -2,9 +2,11 @@ import { InstagramCarouselCaptionSuite } from "@/instagram-carousel-caption-suit
 import { InstagramPublishingWorkflow } from "@/instagram-publishing-workflow";
 import { MediaPublishingOverviewPortal } from "@/media-publishing-overview";
 import { PublishingRouteIntent } from "@/publishing-route-intent";
+import { ThreadsPublishingSuite } from "@/threads-publishing-suite";
 import "../media-publishing-overview.css";
 import "../instagram-caption-suite.css";
+import "../threads-publishing.css";
 
 export default function PublishingLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}<MediaPublishingOverviewPortal/><InstagramPublishingWorkflow/><InstagramCarouselCaptionSuite/><PublishingRouteIntent/></>;
+  return <>{children}<ThreadsPublishingSuite/><MediaPublishingOverviewPortal/><InstagramPublishingWorkflow/><InstagramCarouselCaptionSuite/><PublishingRouteIntent/></>;
 }
