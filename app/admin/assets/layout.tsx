02d2bd025ab0126831ba5a3b394acf6e596d5cb4
Elicasta <130819591+Elicasta@users.noreply.github.com>
2026-08-15 04:14:16 -0400

    style: mount Pathway Assets workspace styles

---
+++ app/admin/assets/layout.tsx
@@ -0,0 +1,6 @@
+import "../pathway-asset-library.css";
+import "../pathway-asset-library-v2.css";
+
+export default function PathwayAssetsLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
