de6a83ce0c05931b18775e443da5cdb5c72338dd
Elicasta <130819591+Elicasta@users.noreply.github.com>
2026-08-16 15:31:29 -0400

    Deep-link legacy calendar into master Publishing calendar view

--- app/admin/content-calendar/page.tsx
+++ app/admin/content-calendar/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function AdminContentCalendarPage() {
-  redirect("/admin/publishing");
+  redirect("/admin/publishing?view=calendar");
 }
